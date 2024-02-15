a9ffd5d0a08b23d913f45a214b6163cf940caad3
Ina <ina9377@gmail.com>
2024-02-15 20:15:35 +0900

    기본 animation

--- interactive-3d-scroll/src/routes/root.jsx
+++ interactive-3d-scroll/src/routes/root.jsx
@@ -1,28 +1,8 @@
 const Root = () => {
   return (
     <div>
-      <h1>CSS Transform</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
-        sequi dolores quibusdam iste commodi eligendi repellendus fugit cumque
-        consequuntur ratione, aut cupiditate soluta error reiciendis rem
-        sapiente dignissimos, quam fuga? Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Sit dignissimos, sapiente illum magnam labore delectus
-        ducimus, perferendis quia quas voluptate aliquam fugiat excepturi odit,
-        non soluta provident facere aspernatur nisi!
-      </p>
-      <div className="box-container">
-        <div className="box">A</div>
-        <div className="box">B</div>
-        <div className="box">C</div>
-        <div className="box">D</div>
-        <div className="box">E</div>
-      </div>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam unde
-        nam, quisquam illo porro sed natus quas mollitia, ad vel magnam
-        provident quasi? Excepturi sequi libero rem nostrum placeat error!
-      </p>
+      <h1>CSS Animation</h1>
+      <div className="box">BOX</div>
     </div>
   );
 };
