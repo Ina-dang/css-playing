ea3eeec9062f28e2547e1ffb719841818e4cc046
Ina <ina9377@gmail.com>
2024-02-18 20:46:28 +0900

    3D 뒷모습 보이지않게하기

--- interactive-3d-scroll/src/routes/root.jsx
+++ interactive-3d-scroll/src/routes/root.jsx
@@ -4,9 +4,10 @@ const Root = () => {
       {/* frame by frame animation */}
       <div className="spaceship"></div>
       <div className="world">
-        <div className="card">CARD</div>
-        <div className="card">CARD</div>
-        <div className="card">CARD</div>
+        <div className="card">
+          <div className="card-side card-side-front">Front</div>
+          <div className="card-side card-side-back">Back</div>
+        </div>
       </div>
     </div>
   );
