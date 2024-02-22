9214dddb0fd76c9c927751a70c370462c5c6d5dd
Ina <ina9377@gmail.com>
2024-02-22 20:45:08 +0900

    클릭 이벤트 추가

--- interactive-3d-scroll/src/pages/ThreeDoors.jsx
+++ interactive-3d-scroll/src/pages/ThreeDoors.jsx
@@ -1,25 +1,32 @@
+import { useState } from "react";
+
 const ThreeDoors = () => {
+  const doors = [1, 2, 3];
+  const [open, setOpen] = useState([false, false, false]);
+
+  const handleDoor = (index) => () => {
+    console.log(index);
+    setOpen((prev) => {
+      const updateOpenStatus = [...prev];
+      updateOpenStatus[index] = !updateOpenStatus[index];
+      return updateOpenStatus;
+    });
+  };
   return (
     <div className="three-doors">
       <div className="stage">
-        <div className="door">
-          <div className="door-back">
-            <div className="ilbuni" />
+        {doors.map((item, index) => (
+          <div
+            className={open[index] ? "door door-opened" : "door"}
+            key={item}
+            onClick={handleDoor(index)}
+          >
+            <div className="door-back">
+              <div className="ilbuni" />
+            </div>
+            <div className="door-body" />
           </div>
-          <div className="door-body" />
-        </div>
-        <div className="door">
-          <div className="door-back">
-            <div className="ilbuni" />
-          </div>
-          <div className="door-body" />
-        </div>
-        <div className="door">
-          <div className="door-back">
-            <div className="ilbuni" />
-          </div>
-          <div className="door-body" />
-        </div>
+        ))}
       </div>
     </div>
   );
